8c41d81874bcf96063233b675b13e90482d95142
a <a@as-Virtual-Machine.local>
2025-11-08 22:37:05 -0800

    Add install method to copy files into libexec and create wrapper script

--- spam.rb
+++ spam.rb
@@ -4,4 +4,11 @@
 class Spam < Formula
   url 'https://github.com/8ta4/spam/releases/download/v0.1.0/spam.tar.gz'
   sha256 'a37ad7445d8bb312ed7db82731fae89aed06c05e149014fff80e7228c8732567'
+  def install
+    libexec.install Dir['*']
+    (bin / 'spam').write <<~SHELL
+      #!/bin/bash
+      exec "#{Formula['node'].opt_bin}/node" "#{libexec}/target/main.js" "$@"
+    SHELL
+  end
 end
