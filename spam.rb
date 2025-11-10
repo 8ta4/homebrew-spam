92d959d3103a47b978ffa3c5392488253863ddde
a <a@as-Virtual-Machine.local>
2025-11-09 18:13:26 -0800

    Refresh download link and hash

--- spam.rb
+++ spam.rb
@@ -2,8 +2,8 @@
 
 # This class represents a Homebrew formula for the spam project.
 class Spam < Formula
-  url 'https://github.com/8ta4/spam/releases/download/v0.1.1/spam.tar.gz'
-  sha256 'c43c39d344494ad07778bd2fdbb4820b95dea1dd185d7383c020f9073c7fbea3'
+  url 'https://github.com/8ta4/spam/releases/download/v0.1.2/spam.tar.gz'
+  sha256 'eb4c4d167b30e32b3c59ad50ed4b508ae6cd9d5c5d52bf507073506c7bd80cde'
   depends_on 'node'
   depends_on 'temporal'
   def install
